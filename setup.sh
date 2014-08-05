23835895730dfc603b98fc06106c48495d65d6fb
Katsuhiro OGAWA <ko.fivestar@gmail.com>
2014-08-05 12:58:39 +0900

    my.cnf flexibility

--- setup.sh
+++ setup.sh
@@ -3,8 +3,25 @@
 git submodule init
 git submodule update
 
-for dotfile in .gitconfig .gvimrc .lv .my.cnf .screenrc .vimrc .zshrc
+for dotfile in .gitconfig .gvimrc .lv .screenrc .vimrc .zshrc
 do
   rm -fr ~/$dotfile
   ln -vnfs $PWD/$dotfile ~/$dotfile
+done
+
+# mysql
+mycnfpath=$HOME/.my.cnf
+mysqldir=$HOME/.mysql
+
+rm -fr $mycnfpath
+echo "!includedir $mysqldir" > $mycnfpath
+
+if [ ! -d $mysqldir ]; then
+    mkdir -p $mysqldir
+fi
+
+for myfile in default.cnf
+do
+  rm -fr $mysqldir/$myfile
+  ln -vnfs $PWD/.mysql/$myfile $mysqldir/$myfile
 done
